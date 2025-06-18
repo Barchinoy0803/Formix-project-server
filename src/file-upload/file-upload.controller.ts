d95415a93df7ceb0527a98550081414adf0c943a
Barchinoy0803 <barchinoyyusupova33@gmail.com>
2025-06-18 21:28:42 +0500

    file upload logic fixed

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -7,15 +7,18 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CloudinaryService } from 'nestjs-cloudinary';
 import { Express } from 'express';
+import * as multer from 'multer';
 
 @Controller('file-upload')
 export class FileUploadController {
   constructor(private readonly cloudinaryService: CloudinaryService) { }
 
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', {
+    storage: multer.memoryStorage()
+  }))
   async create(@UploadedFile() file: Express.Multer.File) {
     const { url } = await this.cloudinaryService.uploadFile(file);
     return { url };
-  }
+  };
 }
